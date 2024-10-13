f99ce782764221c9e3f3fb7b4f68bbfdecd2f95e
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-10-13 22:08:47 +1100

    fix eval path issue

--- eval.sh
+++ eval.sh
@@ -3,11 +3,11 @@
 set -x
 
 # TODO
-python run_eval.py --base_architecture dinov2_vits_exp --num_prototypes 1000 --resume saved_models/CUB/dinov2_vits_exp-1000-aug/15nopush0.8554.pth
+python run_eval.py --base_architecture dinov2_vits_exp --num_prototypes 1000 --resume saved_models/dinov2_vits_exp-1000-aug/15nopush0.8554.pth
 # TODO
-python run_eval.py --base_architecture dinov2_vits_exp --num_prototypes 2000 --resume saved_models/CUB/dinov2_vits_exp-2000-aug/21nopush0.8602.pth
+python run_eval.py --base_architecture dinov2_vits_exp --num_prototypes 2000 --resume saved_models/dinov2_vits_exp-2000-aug/21nopush0.8602.pth
 
 # TODO
-python run_eval.py --base_architecture dinov2_vitb_exp --num_prototypes 1000 --resume saved_models/CUB/dinov2_vitb_exp-1000-aug/27nopush0.8814.pth
+python run_eval.py --base_architecture dinov2_vitb_exp --num_prototypes 1000 --resume saved_models/dinov2_vitb_exp-1000-aug/27nopush0.8814.pth
 # TODO
-python run_eval.py --base_architecture dinov2_vitb_exp --num_prototypes 2000 --resume saved_models/CUB/dinov2_vitb_exp-2000aug/0nopush0.8830.pth
+python run_eval.py --base_architecture dinov2_vitb_exp --num_prototypes 2000 --resume saved_models/dinov2_vitb_exp-2000aug/0nopush0.8830.pth
