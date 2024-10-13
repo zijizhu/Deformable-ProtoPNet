3b444fcd3e8604b8d4fe6cb37af9c8a71bfa400c
Zhijie Zhu <zhuzhijie1003@outlook.com>
2024-10-13 22:06:39 +1100

    update eval script

--- eval.sh
+++ eval.sh
@@ -3,11 +3,11 @@
 set -x
 
 # TODO
-python run_eval.py --base_architecture dinov2_vits_exp --num_prototypes 1000 --resume saved_models/CUB/dinov2_vits_exp_1000/
+python run_eval.py --base_architecture dinov2_vits_exp --num_prototypes 1000 --resume saved_models/CUB/dinov2_vits_exp-1000-aug/15nopush0.8554.pth
 # TODO
-python run_eval.py --base_architecture dinov2_vits_exp --num_prototypes 2000 --resume saved_models/CUB/dinov2_vits_exp_2000/
+python run_eval.py --base_architecture dinov2_vits_exp --num_prototypes 2000 --resume saved_models/CUB/dinov2_vits_exp-2000-aug/21nopush0.8602.pth
 
 # TODO
-python run_eval.py --base_architecture dinov2_vitb_exp --num_prototypes 1000 --resume saved_models/CUB/dinov2_vitb_exp_1000/
+python run_eval.py --base_architecture dinov2_vitb_exp --num_prototypes 1000 --resume saved_models/CUB/dinov2_vitb_exp-1000-aug/27nopush0.8814.pth
 # TODO
-python run_eval.py --base_architecture dinov2_vitb_exp --num_prototypes 2000 --resume saved_models/CUB/dinov2_vitb_exp_2000/
+python run_eval.py --base_architecture dinov2_vitb_exp --num_prototypes 2000 --resume saved_models/CUB/dinov2_vitb_exp-2000aug/0nopush0.8830.pth
